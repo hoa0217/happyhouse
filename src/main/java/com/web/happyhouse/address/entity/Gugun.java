832eaf6b28d65669b143dab1a8b676dd0580eaae
이수경 <dltnrud987@gmail.com>
2022-08-07 20:49:55 +0900

    [back] #3 시군구코드 엔티티 추가

--- src/main/java/com/web/happyhouse/address/entity/Gugun.java
+++ src/main/java/com/web/happyhouse/address/entity/Gugun.java
@@ -1,4 +1,19 @@
 package com.web.happyhouse.address.entity;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Gugun {
+    @Id
+    private String gugunCode;
+    private String gugunName;
 }
