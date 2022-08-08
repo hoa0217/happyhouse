e7cb3a789bd71f52e0509e7ec39a02f3f99fa013
jeonghwa_heo <gjwjdghk123@gmail.com>
2022-08-09 00:46:44 +0900

    [back] #3 베이스엔티티 오류수정 - regId 제거

--- src/main/java/com/web/happyhouse/base/BaseEntity.java
+++ src/main/java/com/web/happyhouse/base/BaseEntity.java
@@ -7,16 +7,15 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
-
-    @Id @GeneratedValue
-    private Long regId;
 
     @CreatedDate
     @Column(updatable = false)
