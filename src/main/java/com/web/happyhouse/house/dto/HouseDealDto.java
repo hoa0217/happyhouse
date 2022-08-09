f02ce12eac84baf70305cdd4cb7b6f715a23c11b
이수경 <dltnrud987@gmail.com>
2022-08-10 01:07:00 +0900

    [back] #3 house 관련 DTO 생성 및 엔티티 수정

--- src/main/java/com/web/happyhouse/house/dto/HouseDealDto.java
+++ src/main/java/com/web/happyhouse/house/dto/HouseDealDto.java
@@ -1,4 +1,30 @@
 package com.web.happyhouse.house.dto;
 
+import com.web.happyhouse.house.domain.DealType;
+import com.web.happyhouse.house.entity.HouseInfo;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class HouseDealDto {
+
+    private Long houseDealId;           // 거래ID
+    private double area;                // 면적
+    private BigDecimal price;           // 가격(매매가, 전세가, 월세보증금)
+    private BigDecimal rent;            // 월세
+    private String dealYear;            // 거래년
+    private String dealMonth;           // 거래월
+    private String dealDay;             // 거래날짜
+    private String floor;               // 충
+
+    @Enumerated(EnumType.STRING)
+    private DealType dealType;          // 거래유형(매매, 전세, 월세)
+
+    private Long houseInfoId;
 }
--- src/main/java/com/web/happyhouse/house/dto/HouseOptionDto.java
+++ src/main/java/com/web/happyhouse/house/dto/HouseOptionDto.java
@@ -1,4 +1,40 @@
 package com.web.happyhouse.house.dto;
 
+import com.web.happyhouse.house.entity.HouseOnSale;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class HouseOptionDto {
+
+    private Long houseOptionId;             // 옵션ID
+
+    private Long houseOnSaleId;        // 매물ID
+
+    private Boolean electricYn;             // 전기포함유무
+    private Boolean gasYn;                  // 가스포함유무
+    private Boolean waterYn;                // 수도포함유무
+    private Boolean internetYn;             // 인터넷포함유무
+    private Boolean tvConnectYn;
+
+    private Boolean personalBoilYn;
+    private Boolean centerBoilYn;
+    private Boolean localBoilYn;
+
+    private Boolean rfgYn;
+    private Boolean bedYn;
+    private Boolean laundryYn;
+    private Boolean airConditionerYn;
+    private Boolean microwaveYn;
+    private Boolean gasStoveYn;
+    private Boolean inductionYn;
+    private Boolean closetYn;
+
+    private Boolean tvYn;
+    private Boolean elevatorYn;
 }
