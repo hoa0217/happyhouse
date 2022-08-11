2cb07cded662d04cb48f88126fd090299b15bfa9
hyuntae <believe4279@gmail.com>
2022-08-11 23:22:37 +0900

    [front] #4 맵 수정

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,40 +1,56 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 import Select from "../components/select.js";
 import axios from "axios";
 
-export default function Home({ list }) {
-  const [geo, setGeo] = useState(["서울시"]);
-  const [map, setMap] = useState("");
+export default function Home() {
+  const positions = [
+    {
+      title: "카카오",
+      latlng: { lat: 33.450705, lng: 126.570677 },
+    },
+    {
+      title: "생태연못",
+      latlng: { lat: 33.450936, lng: 126.569477 },
+    },
+    {
+      title: "텃밭",
+      latlng: { lat: 33.450879, lng: 126.56994 },
+    },
+    {
+      title: "근린공원",
+      latlng: { lat: 33.451393, lng: 126.570738 },
+    },
+  ];
 
   return (
     <>
-      <button onClick={moveGeo}>search</button>
-      <Map // 지도를 표시할 Container
-      center={{
-        // 지도의 중심좌표
-        lat: 33.450701,
-        lng: 126.570667,
-      }}
-      style={{
-        // 지도의 크기
-        width: "100%",
-        height: "450px",
-      }}
-      level={3} // 지도의 확대 레벨
-    />
+      <Map
+        center={{
+          lat: 33.450701,
+          lng: 126.570667,
+        }}
+        style={{
+          width: "100vw",
+          height: "100vh",
+        }}
+        level={3} 
+      >
+        {positions.map(position => (
+          <MapMarker
+            key={`${position.title}-${position.latlng}`}
+            position={position.latlng}
+            image={{
+              src: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png",
+              size: {
+                width: 24,
+                height: 35,
+              },
+            }}
+            title={position.title}
+          />
+        ))}
+      </Map>
     </>
   );
-}
-
-export async function getStaticProps() {
-  const res = await axios.get(
-    "https://new.land.naver.com/api/regions/list?cortarNo=0000000000"
-  );
-  const regionList = res.data.regionList;
-
-  return {
-    props: {
-      list: regionList,
-    },
-  };
 }
