70f109f3b8905642a6ecf441558fb9f4641d8193
jeonghwa_heo <gjwjdghk123@naver.com>
2022-08-21 16:09:53 +0900

    [back] #11 CrossOrigin 해결

--- src/main/java/com/web/happyhouse/HappyhouseApplication.java
+++ src/main/java/com/web/happyhouse/HappyhouseApplication.java
@@ -2,12 +2,25 @@ package com.web.happyhouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HappyhouseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HappyhouseApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://happy-haapyhouse.herokuapp.com/");
+			}
+		};
 	}
 
 }
