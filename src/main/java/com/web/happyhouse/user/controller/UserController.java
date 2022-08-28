908aa4ab06ba00be4a57f185bf782fa9b8f3b5b8
jeonghwa_heo <gjwjdghk123@naver.com>
2022-08-28 23:15:05 +0900

    [back] #16 URI 수정

--- src/main/java/com/web/happyhouse/user/controller/UserController.java
+++ src/main/java/com/web/happyhouse/user/controller/UserController.java
@@ -16,13 +16,12 @@ import javax.validation.Valid;
 @Api(tags ={"User 관리 Controller"})
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/user")
 public class UserController {
 
     private final UserService userService;
 
     @ApiOperation(value = "로그인한 User 조회", notes="로그인한 User 조회하기")
-    @GetMapping("/get")
+    @GetMapping("/user/get")
     public ResponseDto<UserDto> get(@SessionAttribute(name = SessionConst.LOGIN_USER, required = false) UserDto user){
         // 세션에 회원 데이터가 없으면
         if (user == null) {
@@ -33,21 +32,27 @@ public class UserController {
         return ResponseDto.res(ResponseCode.OK,"USER", user);
     }
 
+    @ApiOperation(value = "User 조회", notes="User 조회하기")
+    @GetMapping("/user/{userId}")
+    public ResponseDto<UserDto> get(@PathVariable("userId") Long userId){
+        return ResponseDto.res(ResponseCode.OK, "회원조회 성공", userService.get(userId));
+    }
+
     @ApiOperation(value = "User 가입", notes="User 가입하기")
-    @PostMapping("/join")
+    @PostMapping("/user")
     public ResponseDto join(@Valid @ModelAttribute("userDto")UserDto userDto){
         userService.join(userDto);
         return ResponseDto.res(ResponseCode.CREATED, "회원가입 성공");
     }
 
     @ApiOperation(value = "User 정보수정", notes="User 정보수정하기(이름, 비밀번호)")
-    @PatchMapping("/update")
+    @PatchMapping("/user/{userId}")
     public ResponseDto<UserDto> update(@Valid @ModelAttribute("userDto")UserDto userDto) {
         return ResponseDto.res(ResponseCode.OK, "회원정보 업데이트 성공", userService.update(userDto));
     }
 
     @ApiOperation(value = "User 탈퇴", notes="User 탈퇴하기")
-    @DeleteMapping("/delete")
+    @DeleteMapping("/user/{userId}")
     public ResponseDto delete(@Valid @ModelAttribute("loginForm")LoginForm loginForm) {
         userService.delete(loginForm.getEmail(), loginForm.getPassword());
         return ResponseDto.res(ResponseCode.OK, "회원탈퇴 완료");
