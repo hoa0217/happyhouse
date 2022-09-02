39a1c7c8d01cf03089f61ad7e36eb03417249312
hyuntae <believe4279@gmail.com>
2022-09-02 21:29:43 +0900

    [front] #18 리스트클릭

--- front/components/showList.js
+++ front/components/showList.js
@@ -5,10 +5,19 @@ export default function ShowList({houseData}){
     if(!houseData) return ;//처음실행할때 undefined 방지 
     
     const {data : {houseOnSaleJEONSEList, houseOnSaleMAEMAEList, houseOnSaleWOLSEList}} = houseData;
-    
     return (
         <div style={{'position':'fixed', 'width':'20vw','height':'100vh','backgroundColor':'white','top':'32px','left':'0px','zIndex':'10'}}>
-            
+            {
+                //매물을 클릭했을 때 detail페이지로 이동
+                houseOnSaleJEONSEList.map((list,id)=> (
+                        <div onClick={() => console.log(list.houseOnSaleId)} key={id}>
+                            <div>욕실수 : {list.bathCount}</div>
+                            <div>거래유형 : {list.dealType}</div>
+                            <div>전용면적 : {list.netLeasableArea}</div>
+                            <div>기타 등등</div>
+                        </div>
+                ))
+            }
         </div>
     )    
 }
