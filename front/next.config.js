01c6dbcd9b3e467993c5993bac2fd27b704c8da6
jiyong LEE <49055628+Jiyong95@users.noreply.github.com>
2022-09-03 15:54:58 +0900

    [front]#2 custom Axios 추가

--- front/next.config.js
+++ front/next.config.js
@@ -1,7 +1,9 @@
+process.env.KAKAO_API_KEY = '51b2609483f18c181b76031c296f8316';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
