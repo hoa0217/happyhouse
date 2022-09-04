a45ebc01dc7ca12cc991fe4701c8cb465397026e
안병진 <byungjin0120@gmail.com>
2022-09-05 01:27:22 +0900

    [front] #14 config > 이미지를 잠깐 가져다 쓰기위해 임시방편으로 이렇게 작업해놓음

--- front/next.config.js
+++ front/next.config.js
@@ -4,6 +4,11 @@ process.env.KAKAO_API_KEY = '51b2609483f18c181b76031c296f8316';
 const nextConfig = {
   // reactStrictMode: true,
   swcMinify: true,
+  // images 추후에 지워야함
+  images: {
+    domains: ['dimg.donga.com'],
+  },
+  // images 추후에 지워야함
 };
 
 module.exports = nextConfig;
