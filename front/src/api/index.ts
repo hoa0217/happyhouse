e5828b6cf6e167f6f2d764ea4c87c388a5e5f267
hyuntae <believe4279@gmail.com>
2022-09-05 22:41:26 +0900

    [front] #18 apt/map,list  rs,vo

--- front/src/api/index.ts
+++ front/src/api/index.ts
@@ -1,7 +1,9 @@
 import { RemoteAddressRepo } from './AddressStore';
+import { RemoteHouseRepo } from './HouseStore';
 
 const apiStore = {
   remoteAddressRepo: new RemoteAddressRepo(),
+  remoteHouseRepo : new RemoteHouseRepo(),
 };
 
 export default apiStore;
