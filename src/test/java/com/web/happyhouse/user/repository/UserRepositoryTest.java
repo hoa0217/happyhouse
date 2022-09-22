65d841badc756aeed3014a02fa20b70e623ec311
jeonghwa_heo <gjwjdghk123@naver.com>
2022-09-22 12:33:05 +0900

    [back] #16 UserTest 수정

--- src/test/java/com/web/happyhouse/user/repository/UserRepositoryTest.java
+++ src/test/java/com/web/happyhouse/user/repository/UserRepositoryTest.java
@@ -1,17 +1,16 @@
 package com.web.happyhouse.user.repository;
 
+import com.web.happyhouse.login.dto.JoinRq;
 import com.web.happyhouse.user.dto.UserRq;
-import com.web.happyhouse.user.entity.Role;
 import com.web.happyhouse.user.entity.User;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,15 +20,17 @@ public class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     @Test
     public void 회원가입() throws Exception{
         // given
-        UserRq userRq = new UserRq();
-        userRq.setEmail("gjwjdghk123@naver.com");
-        userRq.setPassword("123123");
-        userRq.setName("안녕안녕나는정화야");
-        userRq.setRole(Role.USER);
-        User savedUser = userRepository.save(User.toEntity(userRq));
+        JoinRq joinRq = new JoinRq();
+        joinRq.setEmail("gjwjdghk123@naver.com");
+        joinRq.setPassword("123123");
+        joinRq.setName("안녕안녕나는정화야");
+        User savedUser = userRepository.save(joinRq.toEntity(passwordEncoder));
 
         // when
         User repoUser = userRepository.findByEmail("gjwjdghk123@naver.com").get();
