e331e417fc16dde601e64ad2244f44d1e6d52050
BByungs <byungjin0120@gmail.com>
2022-10-09 02:04:44 +0900

    [front] #68 MyApp > SSR Setting

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,39 +1,36 @@
+import React from 'react';
+import { AppProps } from 'next/app';
+
 import 'semantic-ui-css/semantic.min.css';
 import 'antd/dist/antd.css';
-import App from 'next/app';
-import React from 'react';
-import Layout from '@common/layout';
 import '@styles/reset.scss';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { queryClient } from '@query/queryClient';
 
-import { Provider, useSelector } from 'react-redux';
+import { queryClient as DefaultQueryClient } from '@query/queryClient';
+import { Hydrate, QueryClientProvider } from '@tanstack/react-query';
+
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import { store } from '../store';
 
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
+import Layout from '@common/layout';
 
-export let persistor = persistStore(store);
+export const persistor = persistStore(store);
 
-class CustomApp extends App {
-  constructor(props: any) {
-    super(props);
-  }
-  render() {
-    const { Component, pageProps } = this.props;
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => DefaultQueryClient);
 
-    return (
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <QueryClientProvider client={queryClient}>
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
             <Layout>
               <Component {...pageProps} />
             </Layout>
-          </QueryClientProvider>
-        </PersistGate>
-      </Provider>
-    );
-  }
+          </Hydrate>
+        </QueryClientProvider>
+      </PersistGate>
+    </Provider>
+  );
 }
-
-export default CustomApp;
