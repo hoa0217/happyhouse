014902da5041e1967f0cb0c800c7c0854e0a965c
BByungs <byungjin0120@gmail.com>
2022-10-10 11:46:16 +0900

    [front] #70 OneroomDetail > add OneroomDetail page

---
+++ front/src/pages/detail/villa/oneroom/[id].tsx
@@ -0,0 +1,9 @@
+import classNames from 'classnames/bind';
+import styles from '@components/detailPage/detailPage.module.scss';
+const cx = classNames.bind(styles);
+
+const OneroomDetail = () => {
+  return <span>OneRoom Detail</span>;
+};
+
+export default OneroomDetail;
