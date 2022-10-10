66a5c324604938e8f490433e4d68829cdec972bd
BByungs <byungjin0120@gmail.com>
2022-10-10 19:08:17 +0900

    [front] #issue70 HouseQuery> HouseQuery 수정

--- front/src/query/houseQuery.ts
+++ front/src/query/houseQuery.ts
@@ -1,11 +1,8 @@
 import apiStore from '@api';
 import { useQuery } from '@tanstack/react-query';
-import HouseInfoListVO from 'src/domain/vo/house/apt/HouseInfoListVO';
-import MapListVO from 'src/domain/vo/house/apt/MapListVO';
-import OfficeHouseListVO from 'src/domain/vo/house/officetel/OfficeHouseInfoListVO';
-import OfficeMapListVO from 'src/domain/vo/house/officetel/OfficeMapListVO';
-
-const keys = ['house', 'apt'];
+import HouseListVO from 'src/domain/vo/house/list';
+import HouseMapAptListVO from 'src/domain/vo/house/map/apt';
+import HouseMapOfficetelListVO from 'src/domain/vo/house/map/officetel';
 
 export const HouseQueryKeys = {
   HOUSEDETAIL: (houseOnSaleId: number) => ['house', 'detail', houseOnSaleId] as const,
@@ -14,75 +11,52 @@ export const HouseQueryKeys = {
   HOUSEMAPOFFICETEL: (dongCode: string) => ['house', 'map', 'officetel', dongCode] as const,
 };
 
-export const fetchHouseAptMap = async (dongCode: string) => {
-  const { data } = await apiStore.remoteHouseRepo.fetchHouseAptMap(dongCode);
-  return new MapListVO(data);
-};
-
-export const fetchHouseAptList = async (houseInfoId: string) => {
-  const { data } = await apiStore.remoteHouseRepo.fetchHouseAptList(houseInfoId);
-  return new HouseInfoListVO(data);
-};
-
-export function useHouseAptMap(dongCode: string) {
-  return useQuery([...keys, 'map', dongCode], () => fetchHouseAptMap(dongCode));
-}
-
-export function useHouseAptList(houseInfoId: string) {
-  return useQuery([...keys, 'list', houseInfoId], () => fetchHouseAptList(houseInfoId));
-}
-
-export const fetchHouseOfficetelMap = async (dongCode: string) => {
-  const { data } = await apiStore.remoteHouseRepo.fetchHouseOfficetelMap(dongCode);
-  return new OfficeMapListVO(data);
-};
-
-export function useHouseOfficeMap(dongCode: string) {
-  return useQuery(['house', 'officetel', 'map', dongCode], () => fetchHouseOfficetelMap(dongCode));
-}
-
-export const fetchHouseOfficetelList = async (houseInfoId: string) => {
-  const { data } = await apiStore.remoteHouseRepo.fetchHouseOfficetelList(houseInfoId);
-  return new OfficeHouseListVO(data);
-};
-
-export function useHouseOfficeList(houseInfoId: string) {
-  return useQuery(['house', 'officetel', 'list', houseInfoId], () => fetchHouseOfficetelList(houseInfoId));
-}
-
 /**
  * @param houseOnSaleId number
  * @description 클릭한 아파트/오피스텔 매물 상세 정보를 가져오는 쿼리
  */
 export function useHouseDetail(houseOnSaleId: number) {
-  return useQuery(
-    HouseQueryKeys.HOUSEDETAIL(houseOnSaleId),
-    () => apiStore.remoteHouseRepo.fetchHouseDetail(houseOnSaleId),
-    {
-      enabled: !!houseOnSaleId, // houseOnSaleId 값이 있을때만 실횅
-    },
-  );
+  return useQuery(HouseQueryKeys.HOUSEDETAIL(houseOnSaleId), () => fetchHouseDetail(houseOnSaleId), {
+    enabled: !!houseOnSaleId, // houseOnSaleId 값이 있을때만 실횅
+  });
 }
+export async function fetchHouseDetail(houseOnSaleId: number) {
+  const { data } = await apiStore.remoteHouseRepo.fetchHouseDetail(houseOnSaleId);
+  return data;
+}
+
 /**
  * @param houseInfoId number
  * @description 클릭한 아파트/오피스텔 매물 상세 정보를 가져오는 쿼리
  */
 export function useHouseList(houseInfoId: number) {
-  return useQuery(HouseQueryKeys.HOUSELIST(houseInfoId), () => apiStore.remoteHouseRepo.fetchHouseList(houseInfoId));
+  return useQuery(HouseQueryKeys.HOUSELIST(houseInfoId), () => fetchHouseList(houseInfoId));
 }
+export async function fetchHouseList(houseInfoId: number) {
+  const { data } = await apiStore.remoteHouseRepo.fetchHouseList(houseInfoId);
+  return new HouseListVO(data);
+}
+
 /**
  * @param dongCode string
  * @description 동에 있는 아파트 정보 리스트를 가져오는 쿼리
  */
 export function useHouseMapApt(dongCode: string) {
-  return useQuery(HouseQueryKeys.HOUSEMAPAPT(dongCode), () => apiStore.remoteHouseRepo.fetchHouseMapApt(dongCode));
+  return useQuery(HouseQueryKeys.HOUSEMAPAPT(dongCode), () => fetchHouseMapApt(dongCode));
 }
+export async function fetchHouseMapApt(dongCode: string) {
+  const { data } = await apiStore.remoteHouseRepo.fetchHouseMapApt(dongCode);
+  return new HouseMapAptListVO(data);
+}
+
 /**
  * @param dongCode string
- * @description 동에 있는 오피스텔정보 리스트를 가져오는 쿼리
+ * @description 동에 있는 오피스텔 정보 리스트를 가져오는 쿼리
  */
-export function useHouseMapOffietel(dongCode: string) {
-  return useQuery(HouseQueryKeys.HOUSEMAPOFFICETEL(dongCode), () =>
-    apiStore.remoteHouseRepo.fetchHouseMapOfficetel(dongCode),
-  );
+export function useHouseMapOfficetel(dongCode: string) {
+  return useQuery(['house', 'officetel', 'map', dongCode], () => fetchHouseMapOfficetel(dongCode));
+}
+export async function fetchHouseMapOfficetel(dongCode: string) {
+  const { data } = await apiStore.remoteHouseRepo.fetchHouseMapOfficetel(dongCode);
+  return new HouseMapOfficetelListVO(data);
 }
