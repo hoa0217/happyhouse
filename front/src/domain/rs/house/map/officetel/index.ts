b19f5d8634e3c1827a6781020e8052fbdbda9304
안병진 <byungjin0120@gmail.com>
2022-10-10 22:29:50 +0900

    [front] #70 HouseMapOfficetelRs > CommonReponseType 사용

--- front/src/domain/rs/house/map/officetel/index.ts
+++ front/src/domain/rs/house/map/officetel/index.ts
@@ -1,12 +1,10 @@
-import { ResponseCodeType, DongDto, HouseInfoDto } from 'src/domain/rs/commonTypes';
+import { DongDto, HouseInfoDto, CommonReponseType } from 'src/domain/rs/commonTypes';
 
 export interface HouseMapOfficetelDataType {
   dongDto: DongDto;
   houseInfoDtoList: HouseInfoDto[];
 }
 
-export interface HouseMapOfficetelRs {
+export interface HouseMapOfficetelRs extends CommonReponseType {
   data: HouseMapOfficetelDataType;
-  message: string;
-  responseCode: ResponseCodeType;
 }
