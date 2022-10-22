62f200376eef780c4f8581f6a9349df063cb313d
hyuntae <believe4279@gmail.com>
2022-10-22 16:31:00 +0900

    [front] #46 로그인시헤더전환

--- front/src/components/common/layout/header/index.tsx
+++ front/src/components/common/layout/header/index.tsx
@@ -1,7 +1,10 @@
 import { Layout as AntdLayout } from 'antd';
 import classNames from 'classnames/bind';
+import { clear } from 'console';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearToken } from 'src/store/reducer/loginReducer';
 import styles from './header.module.scss';
 
 const cx = classNames.bind(styles);
@@ -9,6 +12,8 @@ const cx = classNames.bind(styles);
 const Header = () => {
   const { Header } = AntdLayout;
   const { pathname } = useRouter();
+  const { token } = useSelector((state: any) => state.login);
+  const dispatch = useDispatch();
 
   return (
     <Header className={cx('header')}>
@@ -38,19 +43,38 @@ const Header = () => {
               <a role="button">투룸</a>
             </Link>
           </li>
+          <li className={cx({ isCurrent: pathname === '/bucket' })}>
+            <Link href="/bucket">
+              <a role="button">내가찜한집</a>
+            </Link>
+          </li>
         </nav>
         {/* login 영역 */}
         <div className={cx('login_Wrap')}>
-          <Link href="/login">
-            <a role="button" className={cx('login')}>
-              로그인
-            </a>
-          </Link>
-          <Link href="/">
-            <a role="button" className={cx('login')}>
-              회원가입
-            </a>
-          </Link>
+          <div className={cx('unLogin', { visible: !token })}>
+            <Link href="/login">
+              <a role="button" className={cx('login')}>
+                로그인
+              </a>
+            </Link>
+            <Link href="/">
+              <a role="button" className={cx('login')}>
+                회원가입
+              </a>
+            </Link>
+          </div>
+          <div className={cx('doLogin', { visible: token })}>
+            <Link href="/mypage">
+              <a role="button" className={cx('login')}>
+                마이페이지
+              </a>
+            </Link>
+            <button onClick={() => dispatch(clearToken())}>
+              <a role="button" className={cx('login')}>
+                로그아웃
+              </a>
+            </button>
+          </div>
         </div>
       </div>
     </Header>
